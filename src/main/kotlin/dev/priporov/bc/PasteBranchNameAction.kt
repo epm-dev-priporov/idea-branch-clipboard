742e0d717d1da4278758cc0964a6e360a31abfe1
Dmitrii_Priporov <devpriporov@mail.ru>
2023-06-29 00:45:34 +0300

    v1.0.0: completed

---
+++ src/main/kotlin/dev/priporov/bc/PasteBranchNameAction.kt
@@ -0,0 +1,56 @@
+package dev.priporov.bc
+
+import com.intellij.openapi.actionSystem.AnAction
+import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.CommonDataKeys
+import com.intellij.openapi.project.Project
+import git4idea.GitUtil
+import git4idea.repo.GitRepository
+import java.awt.Toolkit
+import java.awt.datatransfer.*
+
+class PasteBranchNameAction : AnAction("Paste branch name") {
+
+    override fun actionPerformed(e: AnActionEvent) {
+        val project: Project = e.getData(CommonDataKeys.PROJECT)!!
+        val repositories: MutableCollection<GitRepository> = GitUtil.getRepositories(project)
+
+        if (repositories.isEmpty()) {
+            return
+        }
+
+        val currentBranch = repositories.first().currentBranch?.name ?: ""
+        val clipboard: Clipboard = Toolkit.getDefaultToolkit().systemClipboard
+        val clipboardOwner = ClipboardOwner { _, _ -> }
+        clipboard.setContents(BranchNameTransferable(currentBranch), clipboardOwner)
+
+        println(currentBranch)
+    }
+
+    override fun update(e: AnActionEvent) {
+        super.update(e)
+    }
+
+}
+
+class BranchNameTransferable(private val data: String) : Transferable, ClipboardOwner {
+
+    private val dataFlavors = arrayOf(DataFlavor.stringFlavor)
+
+    override fun getTransferDataFlavors(): Array<DataFlavor> = dataFlavors
+
+    override fun isDataFlavorSupported(flavor: DataFlavor?): Boolean = flavor == DataFlavor.stringFlavor
+
+    override fun getTransferData(flavor: DataFlavor?): Any {
+        if (!isDataFlavorSupported(flavor)) {
+            throw UnsupportedFlavorException(flavor)
+        }
+        if (DataFlavor.stringFlavor == flavor) {
+            return data
+        }
+        return data
+    }
+
+    override fun lostOwnership(clipboard: Clipboard?, contents: Transferable?) {}
+
+}
